4eeaa14553aafef2abb832bbd1134e3aef0949b7
kimulu <kimulumichael@gmail.com>
2024-02-19 03:56:49 -0800

    new commit

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,38 +1,48 @@
 #include "sort.h"
 
 /**
- * insertion_sort_list - sorts a doubly linked list of integers
- * in ascending order
- * using the Insertion sort algorithm
- * @list: double pointer to the head of the linked list
+ * swap_nodes_list - Swap nodes in a listint_t doubly-linked list.
+ * @h: A pointer to the head of the doubly-linked list.
+ * @n1: A pointer to the first node to swap.
+ * @n2: The second node to swap.
+ */
+void swap_nodes_list(listint_t **h, listint_t **n1, listint_t *n2)
+{
+(*n1)->next = n2->next;
+if (n2->next != NULL)
+n2->next->prev = *n1;
+n2->prev = (*n1)->prev;
+n2->next = *n1;
+if ((*n1)->prev != NULL)
+(*n1)->prev->next = n2;
+else
+*h = n2;
+(*n1)->prev = n2;
+*n1 = n2->prev;
+}
+
+/**
+ * insertion_sort_list - Sorts a doubly linked list of integers
+ * using the insertion sort algorithm.
+ * @list: A pointer to the head of a doubly-linked list of integers.
+ *
+ * Description: Prints the list after each swap.
  */
 void insertion_sort_list(listint_t **list)
 {
-listint_t *curr, *prev, *next;
-curr = (*list)->next;
+listint_t *iter, *insert, *tmp;
 
 if (list == NULL || *list == NULL || (*list)->next == NULL)
 return;
 
-while (curr != NULL)
+for (iter = (*list)->next; iter != NULL; iter = tmp)
 {
-next = curr->next;
-prev = curr->prev;
-while (prev != NULL && prev->n > curr->n)
+tmp = iter->next;
+insert = iter->prev;
+while (insert != NULL && iter->n < insert->n)
 {
-prev->next = curr->next;
-if (curr->next != NULL)
-curr->next->prev = prev;
-curr->prev = prev->prev;
-curr->next = prev;
-if (prev->prev != NULL)
-prev->prev->next = curr;
-else
-*list = curr;
-prev->prev = curr;
-prev = curr->prev;
+swap_nodes_list(list, &insert, iter);
 print_list(*list);
 }
-curr = next;
 }
 }
