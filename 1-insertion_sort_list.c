4a75c4e3a787f155ab6f65cee0d5e753321489d2
kimulu <kimulumichael@gmail.com>
2024-02-19 03:49:56 -0800

    new commit

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,49 +1,38 @@
 #include "sort.h"
-#include <stdio.h>
 
 /**
- * insertion_sort_list - Sorts a doubly linked list of integers in ascending
- * order using the Insertion sort algorithm.
- * @list: A pointer to the pointer to the head of the linked list.
+ * insertion_sort_list - sorts a doubly linked list of integers
+ * in ascending order
+ * using the Insertion sort algorithm
+ * @list: double pointer to the head of the linked list
  */
 void insertion_sort_list(listint_t **list)
 {
-/*Check if list is NULL or if it contains less than 2 elements*/
+listint_t *curr, *prev, *next;
+curr = (*list)->next;
+
 if (list == NULL || *list == NULL || (*list)->next == NULL)
 return;
 
-listint_t *current = (*list)->next;
-
-/*Main sorting loop*/
-while (current != NULL)
+while (curr != NULL)
 {
-/*Inner loop for insertion*/
-listint_t *prev_node = current->prev;
-listint_t *next_node = current->next;
-
-while (prev_node != NULL && prev_node->n > current->n)
+next = curr->next;
+prev = curr->prev;
+while (prev != NULL && prev->n > curr->n)
 {
-/*Swap nodes*/
-if (prev_node->prev != NULL)
-prev_node->prev->next = current;
-if (current->next != NULL)
-current->next->prev = prev_node;
-
-prev_node->next = next_node;
-current->prev = prev_node->prev;
-
-prev_node->prev = current;
-current->next = prev_node;
-
-if (current->prev == NULL)
-*list = current;
-
-/*Print list after each swap*/
+prev->next = curr->next;
+if (curr->next != NULL)
+curr->next->prev = prev;
+curr->prev = prev->prev;
+curr->next = prev;
+if (prev->prev != NULL)
+prev->prev->next = curr;
+else
+*list = curr;
+prev->prev = curr;
+prev = curr->prev;
 print_list(*list);
-
-prev_node = current->prev;
 }
-
-current = next_node;
+curr = next;
 }
 }
